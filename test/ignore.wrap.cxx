dd779d7e1a53dd76844e8c2cf37bcf37481779ad
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-12 13:00:28 -0500

    forgot some wrap.cxx files for new tests

---
+++ test/ignore.wrap.cxx
@@ -0,0 +1,126 @@
+
+// C.f. https://numpy.org/doc/1.21/reference/c-api/array.html#importing-the-api
+#define PY_ARRAY_UNIQUE_SYMBOL _cpp2py_ARRAY_API
+#ifdef __clang__
+// #pragma clang diagnostic ignored "-W#warnings"
+#endif
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#pragma GCC diagnostic ignored "-Wcpp"
+#endif
+
+#define C2PY_VERSION_MAJOR 0
+#define C2PY_VERSION_MINOR 1
+
+#include "c2py/c2py.hpp"
+#include "ignore.cpp"
+
+using c2py::operator"" _a;
+
+// ==================== Wrapped classes =====================
+
+#ifndef C2PY_HXX_DECLARATION_ignore_GUARDS
+#define C2PY_HXX_DECLARATION_ignore_GUARDS
+template <> constexpr bool c2py::is_wrapped<a_struct> = true;
+#endif
+
+// ==================== enums =====================
+
+// ==================== module classes =====================
+
+template <> inline const std::string c2py::cpp_name<a_struct>   = "a_struct";
+template <> inline constexpr auto c2py::tp_name<a_struct>       = "ignore.AStruct";
+template <> inline constexpr const char *c2py::tp_doc<a_struct> = R"DOC(   * a: int)DOC";
+
+static int synth_constructor_0(PyObject *self, PyObject *args, PyObject *kwargs) {
+  if (args and PyTuple_Check(args) and (PyTuple_Size(args) > 0)) {
+    PyErr_SetString(PyExc_RuntimeError, ("Error in constructing a_struct.\nNo positional arguments allowed. Use keywords arguments"));
+    return -1;
+  }
+  c2py::pydict_extractor de{kwargs};
+  try {
+    ((c2py::wrap<a_struct> *)self)->_c = new a_struct{};
+  } catch (std::exception const &e) {
+    PyErr_SetString(PyExc_RuntimeError, ("Error in constructing a_struct from a Python dict.\n   "s + e.what()).c_str());
+    return -1;
+  }
+  auto &self_c = *(((c2py::wrap<a_struct> *)self)->_c);
+  de("a", self_c.a, false);
+  return de.check();
+}
+
+template <> constexpr initproc c2py::tp_init<a_struct> = synth_constructor_0;
+
+// ----- Method table ----
+template <>
+PyMethodDef c2py::tp_methods<a_struct>[] = {
+
+   {nullptr, nullptr, 0, nullptr} // Sentinel
+};
+
+constexpr auto doc_member_0 = R"DOC()DOC";
+static PyObject *prop_get_dict_0(PyObject *self, void *) {
+  auto &self_c = *(((c2py::wrap<a_struct> *)self)->_c);
+  c2py::pydict dic;
+  dic["a"]           = self_c.a;
+  dic["b"]           = self_c.b;
+  dic["x_ignore_me"] = self_c.x_ignore_me;
+  return dic.new_ref();
+}
+
+// ----- Method table ----
+
+template <>
+constinit PyGetSetDef c2py::tp_getset<a_struct>[] = {c2py::getsetdef_from_member<&a_struct::a, a_struct>("a", doc_member_0),
+                                                     {"__dict__", (getter)prop_get_dict_0, nullptr, "", nullptr},
+                                                     {nullptr, nullptr, nullptr, nullptr, nullptr}};
+
+// ==================== module functions ====================
+
+//--------------------- module function table  -----------------------------
+
+static PyMethodDef module_methods[] = {
+   {nullptr, nullptr, 0, nullptr} // Sentinel
+};
+
+//--------------------- module struct & init error definition ------------
+
+//// module doc directly in the code or "" if not present...
+/// Or mandatory ?
+static struct PyModuleDef module_def = {PyModuleDef_HEAD_INIT,
+                                        "ignore",          /* name of module */
+                                        R"RAWDOC()RAWDOC", /* module documentation, may be NULL */
+                                        -1,                /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+                                        module_methods,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL};
+
+//--------------------- module init function -----------------------------
+
+extern "C" __attribute__((visibility("default"))) PyObject *PyInit_ignore() {
+
+  if (not c2py::check_python_version("ignore")) return NULL;
+
+    // import numpy iff 'numpy/arrayobject.h' included
+#ifdef Py_ARRAYOBJECT_H
+  import_array();
+#endif
+
+  PyObject *m;
+
+  if (PyType_Ready(&c2py::wrap_pytype<c2py::py_range>) < 0) return NULL;
+  if (PyType_Ready(&c2py::wrap_pytype<a_struct>) < 0) return NULL;
+
+  m = PyModule_Create(&module_def);
+  if (m == NULL) return NULL;
+
+  auto &conv_table = *c2py::conv_table_sptr.get();
+
+  conv_table[std::type_index(typeid(c2py::py_range)).name()] = &c2py::wrap_pytype<c2py::py_range>;
+  CLAIR_C2PY_ADD_TYPE_OBJECT(a_struct, "AStruct");
+
+  return m;
+}
