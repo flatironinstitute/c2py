b569f40017b29ef4035187a8ec25e11156bd00c4
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-11-14 07:56:12 -0500

    Add the any test back, simplified

---
+++ test/any.cpp
@@ -0,0 +1,28 @@
+#include <c2py/c2py.hpp>
+#include <c2py/converters/stl/any.hpp>
+#include <complex>
+
+struct opaque {
+  int c                = 17; // not exposed
+  int *non_convertible = 0;
+  int f(int x) { return x + 1; }; // not exposed
+};
+
+// The convertion to Py is done as a PyCapsule of std::any
+template <> struct c2py::py_converter<opaque> : c2py::py_converter_as_any<opaque> {};
+
+// maker
+opaque make_opaque() { return {}; }
+
+// passing the object back from Python
+int take_opaque(opaque const &x) { return x.c; }
+
+// we can modify the object passed from Python
+int inc_opaque(opaque &x) { return ++(x.c); }
+
+// Should not be needed.
+// If a class alaready has a converter, it is automatically rejected
+// from the list of classes to wrap.
+//namespace c2py_module {
+//  constexpr auto reject_names = "opaque";
+//}
