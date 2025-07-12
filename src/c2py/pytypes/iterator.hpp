f96cc73ed07563e73f4817bedae504422f44e6df
Olivier Parcollet <oparcollet@flatironinstitute.org>
2025-07-11 20:25:41 -0400

    Silence warning

--- src/c2py/pytypes/iterator.hpp
+++ src/c2py/pytypes/iterator.hpp
@@ -100,6 +100,12 @@ namespace c2py {
         0,                                                   // tp_version_tag
         0,                                                   // tp_finalize
         0,                                                   // tp_vectorcall
+#if (PY_MINOR_VERSION >= 12)
+        0, // tp_watched
+#endif
+#if (PY_MINOR_VERSION >= 13)
+        0, // tp_versions_used
+#endif
   };
 
   // ---------------------------------------
