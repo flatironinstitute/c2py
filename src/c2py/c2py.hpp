07aa347ca04d2ee0950a5528456d8db05db52c70
Olivier Parcollet <oparcollet@flatironinstitute.org>
2025-07-16 13:47:25 -0400

    Fix tuple bug
    - forgot to include a header in c2py.hpp
    - started a new test to check std container (to be continued)

--- src/c2py/c2py.hpp
+++ src/c2py/c2py.hpp
@@ -53,6 +53,7 @@
 #include "converters/stl/map.hpp"
 #include "converters/stl/optional.hpp"
 #include "converters/stl/pair.hpp"
+#include "converters/stl/tuple.hpp"
 #include "converters/stl/set.hpp"
 #include "converters/stl/array.hpp"
 #include "converters/stl/string.hpp"
